4dd21cb2123f3fb4c5cf7a51ba53b675c819bb0a
jejikeh <jejikeh@gmail.com>
2022-08-13 23:20:52 +0300

    change prism theme

--- src/_bundle/main.js
+++ src/_bundle/main.js
@@ -7,8 +7,6 @@ if (
         window.matchMedia("(prefers-color-scheme: dark)").matches)
 ) {
     document.documentElement.classList.add("dark")
-    console.log("hello world dark")
 } else {
     document.documentElement.classList.remove("dark")
-    console.log("hello world light")
 }
