65c1b94ed84e79a1b94b0a9ffd485b8cd1366245
jejikeh <jejikeh@gmail.com>
2022-08-14 14:36:14 +0300

    udpate theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         extend: {
             colors: {
                 white: "#F5F2F2",
-                black: "#262222",
+                black: "#0D0C0C",
                 gray: "#969696",
             },
             fontFamily: {
